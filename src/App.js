03ece5f03b56541f355300802dd9846b4fe5e4ca
Sol <solcis.bio@gmail.com>
2022-01-04 22:49:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const listaSailor = [
+    {
+      name: "Sailor Saturn",
+      img: "https://nobleorderbrewing.com/img/lists/07/sailor-moon-10-things-you-didnt-know-about-sailor-saturn.jpg",
+      identity: "Hotaru Tomoe"
+    },
+    {
+      name: "Sailor Pluto",
+      img: "https://sm.ign.com/t/ign_latam/screenshot/default/sailor-plut-amor-secreto_94u5.1280.jpg",
+      identity: "Setsuna Meiou"
+    },
+    {
+      name: "Sailor Neptune",
+      img: "http://3.bp.blogspot.com/-1ZfrL5601yU/VHK9nKD9xNI/AAAAAAAAGYI/7Blv5T-G2n4/s1600/Sailor_Neptune.jpg",
+      identity: "Michiru Kaiou"
+    },
+    {
+      name: "Sailor Uranus",
+      img: "https://i.pinimg.com/originals/19/56/fe/1956fe305cddd28dc9364de6683581b6.jpg",
+      identity: "Haruka Tenou"
+    },
+    {
+      name: "Black Lady",
+      img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/06/black-lady-2.jpg?fit=1280%2C720&quality=80&ssl=1",
+      identity: "Rini"
+    },
+    {
+      name: "Sailor Moon",
+      img: "https://static1.abc.es/media/play/2021/04/28/sailor-moon-krZC--1200x630@abc.jpg",
+      identity: "Usagi Tsukino"
+    },
+    {
+      name: "Sailor Mars",
+      img: "https://static2.srcdn.com/wordpress/wp-content/uploads/2017/05/sailor-mars.jpg?q=50&fit=crop&w=737&h=368&dpr=1.5",
+      identity: "Rei Hino"
+    },
+    {
+      name: "Sailor Jupiter",
+      img: "https://nerdcast.net/wp-content/uploads/2020/04/sailor-jupiter.jpg",
+      identity: "Lita Kino"
+    }
+
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
