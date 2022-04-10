09c68fbcfc605f58648a7276088cded18745af27
Sol Turipe <solcis.bio@gmail.com>
2022-04-10 21:17:42 +0200

    I added a new sailor

--- src/service/data.js
+++ src/service/data.js
@@ -39,6 +39,11 @@ const data = [
     img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-3.png?resize=1280%2C720&quality=80&ssl=1",
     identity: "Lita Kino",
   },
+  {
+    name: "Sailor Venus",
+    img: "https://www.filo.news/__export/1571722180724/sites/claro/img/2019/10/22/eternal_sailor_venus_by_bloom2-d7mhh2y-808x454.png_759710130.png",
+    identity: "Minako Aino",
+  },
 ];
 
 export default data;
