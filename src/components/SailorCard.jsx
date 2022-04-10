a20c3ad4481a3c5f273da82ad5da8ec2d3e0110c
Sol Turipe <solcis.bio@gmail.com>
2022-04-10 20:54:35 +0200

    I separated everything by components

--- src/components/SailorCard.jsx
+++ src/components/SailorCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./App.css";
+import "./sailorCard.css";
 
-const Sailor = (props) => {
+const SailorCard = (props) => {
   return (
     <div className="sailorCard">
       <div className="name">{props.name}</div>
-      <div className="identity">Identity: {props.identity}</div>
       <img className="imagen" src={props.img} alt="" />
+      <div className="identity">Identity: {props.identity}</div>
     </div>
   );
 };
 
-export default Sailor;
+export default SailorCard;
---
+++ src/service/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    name: "Sailor Saturn",
+    img: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-7.png?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Hotaru Tomoe",
+  },
+  {
+    name: "Sailor Pluto",
+    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-6.png?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Setsuna Meiou",
+  },
+  {
+    name: "Sailor Neptune",
+    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-1.jpg?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Michiru Kaiou",
+  },
+  {
+    name: "Sailor Uranus",
+    img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-5.png?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Haruka Tenou",
+  },
+  {
+    name: "Black Lady",
+    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/06/black-lady-2.jpg?fit=1280%2C720&quality=80&ssl=1",
+    identity: "Rini",
+  },
+  {
+    name: "Sailor Moon",
+    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-3.jpg?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Usagi Tsukino",
+  },
+  {
+    name: "Sailor Mars",
+    img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-4.png?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Rei Hino",
+  },
+  {
+    name: "Sailor Jupiter",
+    img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-3.png?resize=1280%2C720&quality=80&ssl=1",
+    identity: "Lita Kino",
+  },
+];
+
+export default data;
