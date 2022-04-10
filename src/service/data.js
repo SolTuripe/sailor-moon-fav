d050f9999d103bbc58be63bfe8ae3a21429ba803
Sol Turipe <solcis.bio@gmail.com>
2022-04-10 22:07:52 +0200

    I changed images

--- src/service/data.js
+++ src/service/data.js
@@ -1,32 +1,32 @@
 const data = [
   {
     name: "Sailor Saturn",
-    img: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-7.png?resize=1280%2C720&quality=80&ssl=1",
+    img: "https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2021/02/supersailorsaturn.png",
     identity: "Hotaru Tomoe",
   },
   {
-    name: "Sailor Pluto",
-    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-6.png?resize=1280%2C720&quality=80&ssl=1",
-    identity: "Setsuna Meiou",
+    name: "Black Lady",
+    img: "https://www.tfw2005.com/boards/attachments/795115c1-1aad-447e-8395-6ff002575e35-png.28848266/",
+    identity: "Rini",
   },
   {
     name: "Sailor Neptune",
-    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-1.jpg?resize=1280%2C720&quality=80&ssl=1",
+    img: "http://3.bp.blogspot.com/-1ZfrL5601yU/VHK9nKD9xNI/AAAAAAAAGYI/7Blv5T-G2n4/s1600/Sailor_Neptune.jpg",
     identity: "Michiru Kaiou",
   },
   {
     name: "Sailor Uranus",
-    img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-5.png?resize=1280%2C720&quality=80&ssl=1",
+    img: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2020/12/sailor-uranus-1.jpg?resize=640%2C480&quality=80&ssl=1",
     identity: "Haruka Tenou",
   },
   {
-    name: "Black Lady",
-    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/06/black-lady-2.jpg?fit=1280%2C720&quality=80&ssl=1",
-    identity: "Rini",
+    name: "Sailor Pluto",
+    img: "https://laverdadnoticias.com/export/sites/laverdad/img/2020/11/08/sailor_pluto_clasico_anime_sailor_moon.png_1112373203.png",
+    identity: "Setsuna Meiou",
   },
   {
     name: "Sailor Moon",
-    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-3.jpg?resize=1280%2C720&quality=80&ssl=1",
+    img: "https://dam.tbg.com.mx/content/dam/tbg/mexico/caras/mx/el-espia/17/07/12/sailor-moon-serena-bunny.jpg.imgo.jpg",
     identity: "Usagi Tsukino",
   },
   {
@@ -36,12 +36,12 @@ const data = [
   },
   {
     name: "Sailor Jupiter",
-    img: "https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/04/Sailor-Moon-Este-es-el-ranking-de-poder-de-las-Sailor-Scouts-3.png?resize=1280%2C720&quality=80&ssl=1",
+    img: "https://i.ytimg.com/vi/l3YvEx-mQdI/maxresdefault.jpg",
     identity: "Lita Kino",
   },
   {
     name: "Sailor Venus",
-    img: "https://www.filo.news/__export/1571722180724/sites/claro/img/2019/10/22/eternal_sailor_venus_by_bloom2-d7mhh2y-808x454.png_759710130.png",
+    img: "https://pbs.twimg.com/media/EnShX0ZWMAAoFrm.jpg",
     identity: "Minako Aino",
   },
 ];
